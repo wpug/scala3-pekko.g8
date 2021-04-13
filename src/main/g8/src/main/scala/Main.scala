d1086b94694a8798cd1cdc0e7ea85810e8e22fe1
wpug <w.pawlowski@inf.ug.edu.pl>
2021-04-13 14:36:49 +0200

    Update Main.scala

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,19 +1,21 @@
-import akka.actor.{ActorSystem, Actor, ActorRef, Props}
+object Prog {
+  import akka.actor.{ActorSystem, Actor, ActorRef, Props}
 
-class MyActor extends Actor {
-  def receive: Receive = {
-    case msg => println(s"Odebrałem wiadomość: \${msg}")
+  class MyActor extends Actor {
+    def receive: Receive = {
+      case msg => println(s"Odebrałem wiadomość: \${msg}")
+    }
   }
-}
 
-object Main extends App {
-  val system = ActorSystem("HaloAkka")
-  try {
-    val leonardo = system.actorOf(Props[MyActor](), "leonardo")
-    leonardo ! "Dostałeś Oskara!"
-    println(">>> Naciśnij ENTER żeby zakończyć <<<")
-    io.StdIn.readLine()
-  } finally {
-    system.terminate()
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("HaloAkka")
+    try {
+      val leonardo = system.actorOf(Props[MyActor](), "leonardo")
+      leonardo ! "Dostałeś Oskara!"
+      println(">>> Naciśnij ENTER żeby zakończyć <<<")
+      io.StdIn.readLine()
+    } finally {
+      system.terminate()
+    }
   }
 }
